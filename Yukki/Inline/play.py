a67f3c6204a372da2a7f2b3be952af2c9399c706
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2022-06-25 01:35:51 +0700

    Initial commit

---
+++ Yukki/Inline/play.py
@@ -0,0 +1,249 @@
+from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
+                            InlineKeyboardMarkup, InputMediaPhoto, Message)
+
+from Yukki import db_mem
+
+
+def url_markup(videoid, duration, user_id, query, query_type):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🎵 Mᴜꜱɪᴄ",
+                callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="🎥 Vɪᴅᴇᴏ",
+                callback_data=f"Choose {videoid}|{duration}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="❮",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="• Mᴏʀᴇ",
+                callback_data=f"Search {query}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="❯",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def url_markup2(videoid, duration, user_id):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴜꜱɪᴄ​",
+                callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="• Vɪᴅᴇᴏ​",
+                callback_data=f"Choose {videoid}|{duration}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="• Cʟᴏꜱᴇ",
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
+    ]
+    return buttons
+
+
+def search_markup(
+    ID1,
+    ID2,
+    ID3,
+    ID4,
+    ID5,
+    duration1,
+    duration2,
+    duration3,
+    duration4,
+    duration5,
+    user_id,
+    query,
+):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="『1』", callback_data=f"Yukki {ID1}|{duration1}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="『2』", callback_data=f"Yukki {ID2}|{duration2}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="『3』", callback_data=f"Yukki {ID3}|{duration3}|{user_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="『4』", callback_data=f"Yukki {ID4}|{duration4}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="『5』", callback_data=f"Yukki {ID5}|{duration5}|{user_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="❮", callback_data=f"popat 1|{query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="• Cʟᴏꜱᴇ​", callback_data=f"forceclose {query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="❯", callback_data=f"popat 1|{query}|{user_id}"
+            ),
+        ],
+    ]
+    return buttons
+
+
+def search_markup2(
+    ID6,
+    ID7,
+    ID8,
+    ID9,
+    ID10,
+    duration6,
+    duration7,
+    duration8,
+    duration9,
+    duration10,
+    user_id,
+    query,
+):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="『6』",
+                callback_data=f"Yukki {ID6}|{duration6}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="『7』",
+                callback_data=f"Yukki {ID7}|{duration7}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="『8』",
+                callback_data=f"Yukki {ID8}|{duration8}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="『9』",
+                callback_data=f"Yukki {ID9}|{duration9}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="『10』",
+                callback_data=f"Yukki {ID10}|{duration10}|{user_id}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="❮", callback_data=f"popat 2|{query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="• Cʟᴏꜱᴇ•​", callback_data=f"forceclose {query}|{user_id}"
+            ),
+            InlineKeyboardButton(
+                text="❯", callback_data=f"popat 2|{query}|{user_id}"
+            ),
+        ],
+    ]
+    return buttons
+
+
+def secondary_markup(videoid, user_id):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ", callback_data=f"close"),
+        ],
+    ]
+    return buttons
+
+
+def secondary_markup2(videoid, user_id):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ", callback_data=f"close"),
+        ],
+    ]
+    return buttons
+
+
+def primary_markup(videoid, user_id, current_time, total_time):
+    if videoid not in db_mem:
+        db_mem[videoid] = {}
+    db_mem[videoid]["check"] = 2
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data=f"close"),
+        ],
+    ]
+    return buttons
+
+
+def timer_markup(videoid, user_id, current_time, total_time):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data=f"close"),
+        ],
+    ]
+    return buttons
+
+
+def audio_markup(videoid, user_id, current_time, total_time):
+    if videoid not in db_mem:
+        db_mem[videoid] = {}
+    db_mem[videoid]["check"] = 2
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data=f"close")],
+    ]
+    return buttons
+
+
+def audio_timer_markup_start(videoid, user_id, current_time, total_time):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ", callback_data=f"close")],
+    ]
+    return buttons
+
+
+audio_markup2 = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="• Mᴇɴᴜ​", switch_inline_query_current_chat=""
+            ),
+        ],
+        [InlineKeyboardButton("• Cʟᴏꜱᴇ​", callback_data="close")],
+    ]
+)
---
+++ Yukki/Inline/playlist.py
@@ -0,0 +1,184 @@
+from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
+                            InlineKeyboardMarkup, InputMediaPhoto, Message)
+
+
+def check_markup(user_name, user_id, videoid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Playlist​",
+                callback_data=f"playlist_check {user_id}|Group|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=f"{user_name[:8]}'s Playlist",
+                callback_data=f"playlist_check {user_id}|Personal|{videoid}",
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ", callback_data="close")],
+    ]
+    return buttons
+
+
+def playlist_markup(user_name, user_id, videoid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"{user_name[:8]}'s Playlist",
+                callback_data=f"show_genre {user_id}|Personal|{videoid}",
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data="close")],
+    ]
+    return buttons
+
+
+def play_genre_playlist(user_id, type, videoid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Dj",
+                callback_data=f"play_playlist {user_id}|{type}|Bollywood",
+            ),
+            InlineKeyboardButton(
+                text=f"Sleep",
+                callback_data=f"play_playlist {user_id}|{type}|Hollywood",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"Sad",
+                callback_data=f"play_playlist {user_id}|{type}|Party",
+            ),
+            InlineKeyboardButton(
+                text=f"Party",
+                callback_data=f"play_playlist {user_id}|{type}|Lofi",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="• Bᴀᴄᴋ​",
+                callback_data=f"main_playlist {videoid}|{type}|{user_id}",
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data="close"),
+        ],
+    ]
+    return buttons
+
+
+def add_genre_markup(user_id, type, videoid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"✚ Dj",
+                callback_data=f"add_playlist {videoid}|{type}|Weeb",
+            ),
+            InlineKeyboardButton(
+                text=f"✚ Party",
+                callback_data=f"add_playlist {videoid}|{type}|Sad",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"✚ Sad",
+                callback_data=f"add_playlist {videoid}|{type}|Party",
+            ),
+            InlineKeyboardButton(
+                text=f"✚ Sleep",
+                callback_data=f"add_playlist {videoid}|{type}|Lofi",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="• Bᴀᴄᴋ​​", callback_data=f"goback {videoid}|{user_id}"
+            ),
+            InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data="close"),
+        ],
+    ]
+    return buttons
+
+
+def check_genre_markup(type, videoid, user_id):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Dj", callback_data=f"check_playlist {type}|Weeb"
+            ),
+            InlineKeyboardButton(
+                text=f"Party", callback_data=f"check_playlist {type}|Sad"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"Sad", callback_data=f"check_playlist {type}|Party"
+            ),
+            InlineKeyboardButton(
+                text=f"Sleep", callback_data=f"check_playlist {type}|Lofi"
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ", callback_data="close")],
+    ]
+    return buttons
+
+
+def third_playlist_markup(user_name, user_id, third_name, userid, videoid):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Playlist​",
+                callback_data=f"show_genre {user_id}|Group|{videoid}",
+            ),
+            InlineKeyboardButton(
+                text=f"{user_name[:8]}'s Playlist",
+                callback_data=f"show_genre {user_id}|Personal|{videoid}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{third_name[:16]}'s Playlist",
+                callback_data=f"show_genre {userid}|third|{videoid}",
+            ),
+        ],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data="close")],
+    ]
+    return buttons
+
+
+def paste_queue_markup(url):
+    buttons = [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"resumecb"),
+            InlineKeyboardButton(text="II", callback_data=f"pausecb"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"skipcb"),
+            InlineKeyboardButton(text="▢", callback_data=f"stopcb"),
+        ],
+        [InlineKeyboardButton(text="Checkout Queued Playlist", url=f"{url}")],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data=f"close")],
+    ]
+    return buttons
+
+
+def fetch_playlist(user_name, type, genre, user_id, url):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Play {user_name[:10]}'s {genre} Playlist",
+                callback_data=f"play_playlist {user_id}|{type}|{genre}",
+            ),
+        ],
+        [InlineKeyboardButton(text="Checkout Playlist", url=f"{url}")],
+        [InlineKeyboardButton(text="• Cʟᴏꜱᴇ​", callback_data=f"close")],
+    ]
+    return buttons
+
+
+def delete_playlist_markuup(type, genre):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=f"Yes! Delete",
+                callback_data=f"delete_playlist {type}|{genre}",
+            ),
+            InlineKeyboardButton(text="• Nᴏ", callback_data=f"close"),
+        ],
+    ]
+    return buttons
